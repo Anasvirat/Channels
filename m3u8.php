e3cbd7674de5c3b5d236baf6f9731fce186d90d0
Anasvirat <anasmohammed18745@gmail.com>
2025-05-31 17:32:57 +0530

    Update m3u8.php

--- m3u8.php
+++ m3u8.php
@@ -4,12 +4,12 @@ set_time_limit(0);
 // M3U8 stream URL
 $m3u8_url = "http://exm3u.extraott.com:80/live/2249871453/1991220000/401627870.m3u8";
 
-// Output TS file
-$output_file = "starsports1tamil.ts";
+// Output file path (inside web root)
+$output_file = "/var/www/html/output/starsports1tamil.ts";
 
-echo "Recording from $m3u8_url to $output_file...\n";
+echo "Recording to $output_file from $m3u8_url...\n";
 
-// FFmpeg command to record
+// Run FFmpeg until script stops
 $ffmpeg_cmd = "ffmpeg -i \"$m3u8_url\" -c copy -f mpegts -y \"$output_file\"";
 passthru($ffmpeg_cmd);
 ?>
